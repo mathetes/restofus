31bf343c7982d478f8c47ca9e29525839719a963
mathetes <nsk.roman@gmail.com>
2020-07-21 12:20:54 +0700

    lesson 36 done. Profile fetch data from server

--- app/Main.js
+++ app/Main.js
@@ -16,6 +16,7 @@ import Terms from "./components/Terms";
 import CreatePost from "./components/CreatePost";
 import ViewSinglePost from "./components/ViewSinglePost";
 import FlashMessages from "./components/FlashMessages";
+import Profile from "./components/Profile";
 
 function Main() {
   const initialState = {
@@ -64,6 +65,9 @@ function Main() {
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Switch>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
             <Route path="/" exact>
               {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
